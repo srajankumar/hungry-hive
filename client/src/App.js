ac1de8f56053e073e050b252588ad033bbea5187
srajan <kumarsrajan02@gmail.com>
2023-08-11 19:41:50 +0530

    flowbite added

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <div>
+      <Navbar />
       <h1 className="bg-green-300">hello</h1>
       <h2 className="bg-pink-500">hellloooo</h2>
     </div>
