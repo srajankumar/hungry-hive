8857de16103b734302c8def8db40c73e2f104057
srajan <kumarsrajan02@gmail.com>
2023-08-14 17:30:34 +0530

    cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import Navbar from "./components/Navbar";
 import CreateRecipe from "./pages/create-recipes";
 import SavedRecipes from "./pages/saved-recipes";
-import Auth from "./pages/auth";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
 import Home from "./pages/Home";
@@ -16,7 +15,6 @@ function App() {
           <Navbar />
           <Routes>
             <Route exact path="/" element={<Home />}></Route>
-            {/* <Route exact path="/auth" element={<Auth />}></Route> */}
             <Route exact path="/about-us" element={<AboutUs />}></Route>
             <Route exact path="/login" element={<Login />}></Route>
             <Route exact path="/register" element={<Register />}></Route>
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -73,7 +73,6 @@ const Navbar = () => {
             id="navbar-dropdown"
           >
             <ul className="flex font-bold tracking-wide p-4 md:p-0 mt-4 border rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0  border-gray-700">
-              
               <li>
                 <Link
                   to="saved-recipes"
@@ -95,7 +94,7 @@ const Navbar = () => {
                   to="about-us"
                   className="hover:underline hover:underline-offset-4 focus:underline focus:underline-offset-8 block py-2 pl-3 pr-4 rounded md:hover:bg-transparent"
                 >
-                   About Us
+                  About Us
                 </Link>
               </li>
               <li>
