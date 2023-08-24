001b7e477cb184674efd44b7f05a318a29c434fa
TejasNayak42 <tejasnayakc42@gmail.com>
2023-08-24 19:00:20 +0530

    Users model added

--- server/src/models/Users.js
+++ server/src/models/Users.js
@@ -3,8 +3,7 @@ import mongoose from "mongoose";
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  email: { type: String, required: true },
+  savedRecipes: [{ type: mongoose.Schema.Types.ObjectId, ref: "recipes" }],
 });
 
-// usermodel will be generated based on schema, user collection will be created
 export const UserModel = mongoose.model("users", UserSchema);
