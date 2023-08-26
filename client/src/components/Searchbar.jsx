3ba843375e79bdc55c73337c3a12b4ba80ecedf8
srajan <kumarsrajan02@gmail.com>
2023-08-26 21:06:50 +0530

    searchbar works

---
+++ client/src/components/Searchbar.jsx
@@ -0,0 +1,114 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const Searchbar = () => {
+  const [searchResult, setSearchResult] = useState([]);
+  const [key, setKey] = useState("");
+
+  useEffect(() => {
+    const search = async () => {
+      try {
+        if (!key.trim()) {
+          setSearchResult([]);
+          return;
+        }
+        const res = await axios.get("http://localhost:3001/recipes", {
+          params: { key: key, limit: 5 },
+        });
+
+        // Filter search results based on first letters of recipe names
+        const filteredResults = res.data.filter((recipe) =>
+          recipe.name.toLowerCase().startsWith(key.toLowerCase())
+        );
+
+        setSearchResult(filteredResults);
+        console.log(res);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    search();
+  }, [key]);
+
+  // Clear search results when backspace is pressed and input is empty
+  const handleKeyUp = (e) => {
+    if (e.key === "Backspace" && !key.trim()) {
+      setSearchResult([]);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Searching"
+              value={key}
+              onChange={(e) => setKey(e.target.value)}
+              onKeyUp={handleKeyUp}
+            />
+          </div>
+          {searchResult && searchResult.length > 0 && (
+            <div className="search-result">
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                {searchResult.map((recipe) => (
+                  <section className="body-font text-gray-400" key={recipe._id}>
+                    <div className="container flex justify-center md:px-10 px-5 py-10 ">
+                      <div className="flex flex-wrap -m-4">
+                        <div className="p-4">
+                          <div className="h-full bg-[#272727] bg-opacity-70 rounded-lg overflow-hidden">
+                            <img
+                              className="lg:h-48 h-36 w-full object-cover object-center"
+                              src={recipe.imageUrl}
+                              alt={recipe.name}
+                            />
+                            <div className="p-6">
+                              <h2 className="justify-between items-center mb-3 tracking-widest flex text-xs title-font font-medium text-gray-400">
+                                <h1 className="title-font text-xl font-bold tracking-wider text-gray-100">
+                                  {recipe.name}
+                                </h1>
+                              </h2>
+                              {/* <h1 className="title-font text-xl font-bold tracking-wider text-gray-100 mb-3">
+  {recipe.name}
+</h1> */}
+                              <p className="leading-relaxed mb-3">
+                                {recipe.instructions}
+                              </p>
+                              <div className="flex items-center justify-between flex-wrap ">
+                                <div className="inline-flex items-center md:mb-2 lg:mb-0">
+                                  <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="17"
+                                    height="17"
+                                    className="mr-2 text-[#ffc20d]"
+                                    viewBox="0 0 20 20"
+                                  >
+                                    <path
+                                      fill="currentColor"
+                                      d="M10 20a10 10 0 1 1 0-20a10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16a8 8 0 0 0 0 16zm-1-7.59V4h2v5.59l3.95 3.95l-1.41 1.41L9 10.41z"
+                                    />
+                                  </svg>
+                                  {recipe.cookingTime} min
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>{" "}
+                  </section>
+                ))}
+              </div>
+            </div>
+          )}
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Searchbar;
--- client/src/components/allRecipes.jsx
+++ client/src/components/allRecipes.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { useGetUserID } from "../hooks/useGetUserID";
 import { useCookies } from "react-cookie";
+import Searchbar from "./Searchbar";
 
 import jsPDF from "jspdf";
 
@@ -78,112 +79,117 @@ const AllRecipes = () => {
   const isRecipeSaved = (id) => savedRecipes.includes(id);
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      {recipe.map((recipe) => (
-        <section className="body-font text-gray-400" key={recipe._id}>
-          <div className="container flex justify-center md:px-10 px-5 py-10 ">
-            <div className="flex flex-wrap -m-4">
-              <div className="p-4">
-                <div className="h-full bg-[#272727] bg-opacity-70 rounded-lg overflow-hidden">
-                  <img
-                    className="lg:h-48 h-36 w-full object-cover object-center"
-                    src={recipe.imageUrl}
-                    alt={recipe.name}
-                  />
-                  <div className="p-6">
-                    <h2 className="justify-between items-center mb-3 tracking-widest flex text-xs title-font font-medium text-gray-400">
-                      <h1 className="title-font text-xl font-bold tracking-wider text-gray-100">
-                        {recipe.name}
-                      </h1>
-                      <button
-                        onClick={() => saveRecipe(recipe._id)}
-                        disabled={isRecipeSaved(recipe._id)}
-                      >
-                        {isRecipeSaved(recipe._id) ? (
-                          <div className="text-red-500">
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              width="30"
-                              height="30"
-                              viewBox="0 0 24 24"
-                            >
-                              <path
-                                fill="currentColor"
-                                d="M8.4 5.25c-2.78 0-5.15 2.08-5.15 4.78c0 1.863.872 3.431 2.028 4.73c1.153 1.295 2.64 2.382 3.983 3.292l2.319 1.57a.75.75 0 0 0 .84 0l2.319-1.57c1.344-.91 2.83-1.997 3.982-3.292c1.157-1.299 2.029-2.867 2.029-4.73c0-2.7-2.37-4.78-5.15-4.78c-1.434 0-2.695.672-3.6 1.542c-.905-.87-2.167-1.542-3.6-1.542Z"
-                              />
-                            </svg>
-                          </div>
-                        ) : (
-                          <div className="hover:text-red-500 transition duration-300">
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              width="30"
-                              height="30"
-                              viewBox="0 0 24 24"
-                            >
-                              <path
-                                fill="currentColor"
-                                d="M8.4 5.25c-2.78 0-5.15 2.08-5.15 4.78c0 1.863.872 3.431 2.028 4.73c1.153 1.295 2.64 2.382 3.983 3.292l2.319 1.57a.75.75 0 0 0 .84 0l2.319-1.57c1.344-.91 2.83-1.997 3.982-3.292c1.157-1.299 2.029-2.867 2.029-4.73c0-2.7-2.37-4.78-5.15-4.78c-1.434 0-2.695.672-3.6 1.542c-.905-.87-2.167-1.542-3.6-1.542Z"
-                              />
-                            </svg>
-                          </div>
-                        )}
-                      </button>
-                    </h2>
-                    {/* <h1 className="title-font text-xl font-bold tracking-wider text-gray-100 mb-3">
+    <div>
+      <div>
+        <Searchbar />
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {recipe.map((recipe) => (
+          <section className="body-font text-gray-400" key={recipe._id}>
+            <div className="container flex justify-center md:px-10 px-5 py-10 ">
+              <div className="flex flex-wrap -m-4">
+                <div className="p-4">
+                  <div className="h-full bg-[#272727] bg-opacity-70 rounded-lg overflow-hidden">
+                    <img
+                      className="lg:h-48 h-36 w-full object-cover object-center"
+                      src={recipe.imageUrl}
+                      alt={recipe.name}
+                    />
+                    <div className="p-6">
+                      <h2 className="justify-between items-center mb-3 tracking-widest flex text-xs title-font font-medium text-gray-400">
+                        <h1 className="title-font text-xl font-bold tracking-wider text-gray-100">
+                          {recipe.name}
+                        </h1>
+                        <button
+                          onClick={() => saveRecipe(recipe._id)}
+                          disabled={isRecipeSaved(recipe._id)}
+                        >
+                          {isRecipeSaved(recipe._id) ? (
+                            <div className="text-red-500">
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="30"
+                                height="30"
+                                viewBox="0 0 24 24"
+                              >
+                                <path
+                                  fill="currentColor"
+                                  d="M8.4 5.25c-2.78 0-5.15 2.08-5.15 4.78c0 1.863.872 3.431 2.028 4.73c1.153 1.295 2.64 2.382 3.983 3.292l2.319 1.57a.75.75 0 0 0 .84 0l2.319-1.57c1.344-.91 2.83-1.997 3.982-3.292c1.157-1.299 2.029-2.867 2.029-4.73c0-2.7-2.37-4.78-5.15-4.78c-1.434 0-2.695.672-3.6 1.542c-.905-.87-2.167-1.542-3.6-1.542Z"
+                                />
+                              </svg>
+                            </div>
+                          ) : (
+                            <div className="hover:text-red-500 transition duration-300">
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="30"
+                                height="30"
+                                viewBox="0 0 24 24"
+                              >
+                                <path
+                                  fill="currentColor"
+                                  d="M8.4 5.25c-2.78 0-5.15 2.08-5.15 4.78c0 1.863.872 3.431 2.028 4.73c1.153 1.295 2.64 2.382 3.983 3.292l2.319 1.57a.75.75 0 0 0 .84 0l2.319-1.57c1.344-.91 2.83-1.997 3.982-3.292c1.157-1.299 2.029-2.867 2.029-4.73c0-2.7-2.37-4.78-5.15-4.78c-1.434 0-2.695.672-3.6 1.542c-.905-.87-2.167-1.542-3.6-1.542Z"
+                                />
+                              </svg>
+                            </div>
+                          )}
+                        </button>
+                      </h2>
+                      {/* <h1 className="title-font text-xl font-bold tracking-wider text-gray-100 mb-3">
                       {recipe.name}
                     </h1> */}
-                    <p className="leading-relaxed mb-3">
-                      {recipe.instructions}
-                    </p>
-                    <div className="flex items-center justify-between flex-wrap ">
-                      <div className="inline-flex items-center md:mb-2 lg:mb-0">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="17"
-                          height="17"
-                          className="mr-2 text-[#ffc20d]"
-                          viewBox="0 0 20 20"
-                        >
-                          <path
-                            fill="currentColor"
-                            d="M10 20a10 10 0 1 1 0-20a10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16a8 8 0 0 0 0 16zm-1-7.59V4h2v5.59l3.95 3.95l-1.41 1.41L9 10.41z"
-                          />
-                        </svg>
-                        {recipe.cookingTime} min
-                      </div>
-                      <button
-                        onClick={() => generatePDF(recipe)}
-                        className="hover:text-white text-[#ffc20d] transition duration-300"
-                      >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="25"
-                          height="25"
-                          viewBox="0 0 16 16"
-                        >
-                          <g
-                            fill="none"
-                            stroke="currentColor"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="1.5"
+                      <p className="leading-relaxed mb-3">
+                        {recipe.instructions}
+                      </p>
+                      <div className="flex items-center justify-between flex-wrap ">
+                        <div className="inline-flex items-center md:mb-2 lg:mb-0">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="17"
+                            height="17"
+                            className="mr-2 text-[#ffc20d]"
+                            viewBox="0 0 20 20"
                           >
-                            <circle cx="4" cy="8" r="2.25" />
-                            <circle cx="12" cy="12" r="2.25" />
-                            <circle cx="12" cy="4" r="2.25" />
-                            <path d="m6 9l4 2M6 7l4-2" />
-                          </g>
-                        </svg>
-                      </button>
+                            <path
+                              fill="currentColor"
+                              d="M10 20a10 10 0 1 1 0-20a10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16a8 8 0 0 0 0 16zm-1-7.59V4h2v5.59l3.95 3.95l-1.41 1.41L9 10.41z"
+                            />
+                          </svg>
+                          {recipe.cookingTime} min
+                        </div>
+                        <button
+                          onClick={() => generatePDF(recipe)}
+                          className="hover:text-white text-[#ffc20d] transition duration-300"
+                        >
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="25"
+                            height="25"
+                            viewBox="0 0 16 16"
+                          >
+                            <g
+                              fill="none"
+                              stroke="currentColor"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              stroke-width="1.5"
+                            >
+                              <circle cx="4" cy="8" r="2.25" />
+                              <circle cx="12" cy="12" r="2.25" />
+                              <circle cx="12" cy="4" r="2.25" />
+                              <path d="m6 9l4 2M6 7l4-2" />
+                            </g>
+                          </svg>
+                        </button>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-        </section>
-      ))}
+          </section>
+        ))}
+      </div>
     </div>
   );
 };
